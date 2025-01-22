56907de0fcbce4988ccb28fe82ef1e4a01b601e3
shabdagya <shabdagya.jh.ug23@nsut.ac.in>
2025-01-22 14:48:16 +0530

    SJ tries to commit

--- app/src/main/java/com/example/reportyourproblem/MainActivity.kt
+++ app/src/main/java/com/example/reportyourproblem/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("karan tries to push")
+                    Greeting("SJ tries to push")
                 }
             }
         }
